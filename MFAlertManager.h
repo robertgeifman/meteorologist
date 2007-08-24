85c0caa7c3b9e2b7aa22ef66f0c489df497d8ad6
joecrow <joecrow@d3ebed13-a016-0410-8795-cbbf129bdbb2>
2007-08-24 04:16:04 +0000

    Starting to organize directory structure with the trunk/branches/tag.
    git-svn-id: https://heat-meteo.svn.sourceforge.net/svnroot/heat-meteo/branches/Meteorologist-1.x@17 d3ebed13-a016-0410-8795-cbbf129bdbb2

---
+++ MFAlertManager.h
@@ -0,0 +1,78 @@
+//
+//  MFAlertManager.h
+//  Meteorologist
+//
+//  Created by Matthew Fahrenbacher on Fri Mar 14 2003.
+//  Copyright (c) 2004 The Meteorologist Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MECity.h"
+#import <Message/NSMailDelivery.h>
+
+@class MFBeeper, MFSongPlayer, MFMessageDisplay, MFEmailer;
+
+@interface MFAlertManager : NSObject 
+{
+    NSMutableArray *alertingCities;
+    
+    IBOutlet MFBeeper *beeper;
+    IBOutlet MFSongPlayer *player;
+    IBOutlet MFMessageDisplay *displayer;
+    IBOutlet MFEmailer *emailer;
+}
+
+- (void)addCity:(NSArray *)array;
+- (void)addCity:(MECity *)city alertOptions:(int)options email:(NSString *)email song:(NSString *)song warning:(NSArray *)warn;
+- (void)removeCity:(MECity *)city;
+
+- (IBAction)kill:(id)sender;
+
+@end
+
+
+@interface MFBeeper : NSObject
+{
+    NSTimer *timer;
+    NSTimer *killer;
+}
+
+- (void)beginBeeping;
+- (void)beep;
+- (void)kill;
+
+@end
+
+
+@interface MFSongPlayer : NSObject
+{
+    NSMovieView *movieView;
+    NSMovie *movie;
+    NSTimer *timer;
+    NSTimer *killer;
+}
+
+- (BOOL)playSong:(NSString *)path;
+- (void)kill;
+
+@end
+
+
+@interface MFMessageDisplay : NSObject
+{
+    IBOutlet NSTextView *message;
+}
+
+- (void)appendMessage:(NSString *)msg;
+- (IBAction)clearLog:(id)sender;
+
+@end
+
+
+@interface MFEmailer : NSObject
+{
+}
+
+- (void)emailMessage:(NSString *)msg toAccount:(NSString *)email;
+
+@end
